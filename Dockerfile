a77ab6df757a08cddded6776959b866b85caf1f9
Luc Juggery <luc.juggery@gmail.com>
2018-01-24 22:55:57 +0100

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM nginx:1.12.2
+COPY . /usr/share/nginx/html
---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+version=$1
+if [ "$version" = "" ]; then
+  echo "version must be provided"
+  exit 1
+fi
+
+# Pull new image
+docker pull
+
+# Stop previous container
+docker stop events
+docker rm events
+
+# Run new one
+docker run -p 80:80 --name events -d lucj/sophia.events:$version
