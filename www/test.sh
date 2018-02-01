8e18ef270b54593eb2fceba2bcbbd78ee06eb86a
Luc Juggery <luc.juggery@gmail.com>
2018-02-01 15:18:33 +0100

    build docker image and run site in an Nginx container

--- www/test.sh
+++ www/test.sh
@@ -1,3 +1,4 @@
-mustache events.json index.mustache > index.html
-
-python -m SimpleHTTPServer 8000
+docker image build -t test.sophia.events .
+ID=$(docker run -d -p 80 test.sophia.events)
+PORT=$(docker inspect --format '{{ (index (index .NetworkSettings.Ports "80/tcp") 0).HostPort }}' $ID)
+echo "=> web site available on http://localhost:$PORT"
