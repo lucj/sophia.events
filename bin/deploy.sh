8b6f8215f4397c6e91741b49d8c518a44e19df33
Luc Juggery <luc.juggery@gmail.com>
2018-02-02 15:16:10 +0100

    Update service instead of container

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,13 +8,5 @@ fi
 # Pull new image
 docker pull lucj/sophia.events:$version
 
-# TODO
-# Replace the following with a service update
-# docker service update www --image=lucj/sophia.events
-
-# Stop previous container
-docker stop events
-docker rm events
-
-# Run new one
-docker run -p 80:80 --name events -d lucj/sophia.events:$version
+# Update www service with the newly retrieved image
+docker service update www --image=lucj/sophia.events:$version
