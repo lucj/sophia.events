de729abf419afa3984c996ec568c5bfcff870325
Herve Nicol <herve.nicol@gmail.com>
2018-03-21 22:20:32 +0100

    scripts fixes: better error handling and shellcheck compliance

--- bin/3-deploy.sh
+++ bin/3-deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-version=$1
+echo_exit() {
+  echo "$*"
+  exit 1
+}
+
+# a sepcific version can be passed as parameter
+version="$1"
 if [ "$version" = "" ]; then
   version="latest"
 fi
 
 # Pull new image
-docker pull lucj/sophia.events:$version
+docker pull lucj/sophia.events:"$version" || echo_exit "Could not pull lucj/sophia.events:$version"
 
 # Update www service with the newly retrieved image
-docker service update sophia_www --image=lucj/sophia.events:$version
+docker service update sophia_www --image=lucj/sophia.events:"$version"
